8126c158b2576fbb12772430a490f274ec32fdbc
XIYANG1018 <pkuyangxi@163.com>
2026-03-23 13:38:21 -0700

    add installment requirement

---
+++ sync-mbert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rsync -avz --progress \
+    -e "ssh -i /Users/xi/Downloads/6140.pem" \
+    --exclude '.git' \
+    --exclude '__pycache__' \
+    --exclude '*.pyc' \
+    --exclude 'checkpoints/' \
+    --exclude 'logs/' \
+    --exclude 'runs/' \
+    ./ \
+    ec2-user@ec2-52-39-93-173.us-west-2.compute.amazonaws.com:/home/ec2-user/project/Multilingual_Code_Switch_Prediction-ML/
